71e614d071e929c650b901252cc3a1c65e4112d5
Joseph <josephsasson1@gmail.com>
2021-07-13 12:59:57 -0400

    Update header before lunch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react'
-// import {Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Header from './Component/Header';
 import GameContainer from './Component/GameContainer'
 import GameWheel from './Component/GameWheel'
