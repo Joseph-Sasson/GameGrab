ceb35ed3101227c7fd0089a07e4e1752becd3084
Joseph <josephsasson1@gmail.com>
2021-07-15 18:07:27 -0400

    add video

--- my-app/src/Component/SingleGame.js
+++ my-app/src/Component/SingleGame.js
@@ -1,4 +1,5 @@
 import {useHistory} from 'react-router-dom'
+import ReactPlayer from 'react-player'
 
 
 function SingleGame(){
@@ -12,17 +13,17 @@ function SingleGame(){
   }
 
   return(
-  <div>
-      <img src={game.image} alt=''/>
-      <p>Name: {game.name}</p>
-      <p>Type: {game.type}</p>
-      <p>Age Limit: {game.ages}</p>
-      <p>Min Players: {game["min players"]}</p>
-      <p>Max Players: {game["max players"]}</p>
-      <p>Play Time: {game.runtime}</p>
-      <p>Rules: {game.rules}</p>
-      <button onClick={goBack}>Return To Home</button>
-  </div>
+    <div>
+        <img src={game.image} alt=''/>
+        <p>Name: {game.name}</p>
+        <p>Type: {game.type}</p>
+        <p>Age Limit: {game.ages}</p>
+        <p>Min Players: {game["min players"]}</p>
+        <p>Max Players: {game["max players"]}</p>
+        <p>Play Time: {game.runtime}</p>
+        <ReactPlayer url={game.rules} />
+        <button onClick={goBack}>Return To Home</button>
+    </div>
 )}
 
 export default SingleGame
